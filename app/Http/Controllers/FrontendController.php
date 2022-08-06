8c4c06f5339e890824057ec12523a4484efa1965
devripon <developershahadathussainripon@gmail.com>
2022-08-06 23:35:07 +0600

    frontend pages links with route done

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -6,8 +6,31 @@ use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
+
+    //Show Login Page
     public function ShowHomePage()
     {
         return view('frontend.index');
     }
+    
+    //Show Login Page
+    public function ShowLoginPage()
+    {
+        return view('frontend.login');
+    }
+
+    //Show Patient Register Page
+    public function RegisterPatientPage()
+    {
+       return view('frontend.patient.register');
+    }
+    
+    //Show Doctor Register pages
+
+    	function RegisterDoctorPage()
+    {
+        return view('frontend.doctor.register');
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 //Frontend Page routes
 Route::get('/',[FrontendController::class,'ShowHomePage']) -> name('home.page');
+Route::get('/login',[FrontendController::class,'ShowLoginPage']) -> name('login.page');
+Route::get('/register-patient',[FrontendController::class,'RegisterPatientPage']) -> name('patient.register.page');
+Route::get('/register-doctor',[FrontendController::class,'RegisterDoctorPage']) -> name('doctor.register.page');
+
+
+
+
+
+
